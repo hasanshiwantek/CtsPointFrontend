994576173c92de6716a6ebb27518a819dd238f74
hasanshiwantek <hasan.shiwantek@gmail.com>
2025-03-14 00:45:19 +0500

    Made TopHeader Component

--- src/app/components/TopHeader.jsx
+++ src/app/components/TopHeader.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "@/style/Header.module.css";
 import Link from "next/link";
+import { IoIosCall } from "react-icons/io";
+import { MdOutlineMail } from "react-icons/md";
 const TopHeader = () => {
   return (
     <>
@@ -8,12 +10,18 @@ const TopHeader = () => {
         <div className={styles.headerSec}>
           <div className={styles.headerContactSec}>
             <ul>
-              <li>
-                <Link href={"/"}>Call +1 (209)-498 4198 </Link>
-              </li>
-              <li>
-                <Link href={"/"}>sales@ctspoint.com</Link>
-              </li>
+              <div className="flex items-center gap-1">
+                <IoIosCall />
+                <li>
+                  <Link href={"/"}>Call +1 (209)-498 4198 </Link>
+                </li>
+              </div>
+              <div className="flex items-center gap-1">
+                <MdOutlineMail />
+                <li>
+                  <Link href={"/"}>sales@ctspoint.com</Link>
+                </li>
+              </div>
             </ul>
           </div>
           <div className={styles.headerText}>
